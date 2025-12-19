5801965c82393ccc1a7a8986fdc9e85b8d79018c
hakuna77 <194330619+hakuna77@users.noreply.github.com>
2025-12-19 16:54:22 +0700

    Revert "mm: batch radix tree operations when truncating pages"
    This reverts commit 1bd8ca9684faf7702f492c0999635ba636037a8b.

--- include/linux/pagemap.h
+++ include/linux/pagemap.h
@@ -16,8 +16,6 @@
 #include <linux/hardirq.h> /* for in_interrupt() */
 #include <linux/hugetlb_inline.h>
 
-struct pagevec;
-
 /*
  * Bits in mapping->flags.
  */
@@ -633,8 +631,6 @@ int add_to_page_cache_lru(struct page *page, struct address_space *mapping,
 extern void delete_from_page_cache(struct page *page);
 extern void __delete_from_page_cache(struct page *page, void *shadow);
 int replace_page_cache_page(struct page *old, struct page *new, gfp_t gfp_mask);
-void delete_from_page_cache_batch(struct address_space *mapping,
-				  struct pagevec *pvec);
 
 /*
  * Like add_to_page_cache_locked, but used to add newly allocated pages:
