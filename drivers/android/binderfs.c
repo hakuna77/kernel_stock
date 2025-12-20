6d5b87dfa1323ac4c0480fd5dd5f561d65269aef
hakuna77 <194330619+hakuna77@users.noreply.github.com>
2025-12-20 15:18:42 +0700

    binder: more Convert to new IDA API

--- drivers/android/binderfs.c
+++ drivers/android/binderfs.c
@@ -117,9 +117,9 @@ static int binderfs_binder_device_create(struct inode *ref_inode,
 	/* Reserve new minor number for the new device. */
 	mutex_lock(&binderfs_minors_mutex);
 	if (++info->device_count <= info->mount_opts.max)
-		minor = ida_simple_get(&binderfs_minors, 0,
-				      use_reserve ? BINDERFS_MAX_MINOR + 1:
-						    BINDERFS_MAX_MINOR_CAPPED + 1,
+		minor = ida_alloc_max(&binderfs_minors,
+				      use_reserve ? BINDERFS_MAX_MINOR :
+						    BINDERFS_MAX_MINOR_CAPPED,
 				      GFP_KERNEL);
 	else
 		minor = -ENOSPC;
@@ -430,9 +430,9 @@ static int binderfs_binder_ctl_create(struct super_block *sb)
 
 	/* Reserve a new minor number for the new device. */
 	mutex_lock(&binderfs_minors_mutex);
-	minor = ida_simple_get(&binderfs_minors, 0,
-			      use_reserve ? BINDERFS_MAX_MINOR  + 1:
-					    BINDERFS_MAX_MINOR_CAPPED + 1,
+	minor = ida_alloc_max(&binderfs_minors,
+			      use_reserve ? BINDERFS_MAX_MINOR :
+					    BINDERFS_MAX_MINOR_CAPPED,
 			      GFP_KERNEL);
 	mutex_unlock(&binderfs_minors_mutex);
 	if (minor < 0) {
